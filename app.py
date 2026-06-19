cbfd683873edcc8fc5aa83e06a731b8964fdbebc
CCesar-c <cesarinfluencer14@gmail.com>
2026-06-19 15:39:37 -0300

    init commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import messagebox
+from banco import cadastrar_produto
+
+
+janela = tk.Tk()
+janela.title("Saep estoque tactil")
+janela.geometry("500x400")
+titulo = tk.Label(
+    janela,
+    text="Saep estoqu tactil",
+    font=("Arial", 18, "bold")
+)
+titulo.pack(pady=10)
+
+tk.Label(janela, text="Nome do produto: ").pack()
+entrada_nome = tk.Entry(janela, width=40)
+entrada_nome.pack()
+
+tk.Label(janela, text="Categoria: ").pack()
+entrada_categoria = tk.Entry(janela, width=40)
+entrada_categoria.pack()
+
+tk.Label(janela, text="Quantidade: ").pack()
+entrada_quantidade = tk.Entry(janela, width=40)
+entrada_quantidade.pack()
+
+tk.Label(janela, text="Preco: ").pack()
+entrada_preco = tk.Entry(janela, width=40)
+entrada_preco.pack()
---
+++ sql.sql
@@ -0,0 +1,11 @@
+create database saep_estoque;
+use saep_estoque;
+CREATE TABLE produto(
+id int auto_increment primary key,
+nome Varchar(100) not null,
+categoria varchar(100) not null,
+quantidade int not null,
+preco decimal(10,2) not null
+);
+
+show tables
