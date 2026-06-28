f3139aff60fc8ec12b4d843f841bdc11c3748632
CCesar-c <lianbastiam@gmail.com>
2026-06-28 15:14:00 -0300

    fixeee

--- app.py
+++ app.py
@@ -1,145 +1,129 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
-from banco import cadastrar_produto, listar_produtos, excluir_produto
+
+# from banco import cadastrar_produto, listar_produtos, excluir_produto
 
 janela = tk.Tk()
-janela.title("Saep estoque tactil")
-janela.geometry("500x400")
-
-frame_botoes = tk.Frame(janela, bg="#f4f6f8")
-frame_botoes.pack(pady=10)
+janela.title("SAEP Estoque Fácil")
+janela.geometry("760x560")
+janela.configure(bg="#f4f6f8")
 
 titulo = tk.Label(
     janela,
-    text="Saep Estoque Facil",
-    font=("Arial", 18, "bold")
+    text="SAEP Estoque Fácil",
+    font=("Arial", 20, "bold"),
+    bg="#f4f6f8",
+    fg="#164193"
 )
 titulo.pack(pady=10)
 
-
-tabela = ttk.Treeview(
+subtitulo = tk.Label(
     janela,
-    columns=("id", "nome", "categoria", "quantidade", "preco"),
-    show="headings"
+    text="Cadastro, listagem e exclusão de produtos",
+    font=("Arial", 11),
+    bg="#f4f6f8",
+    fg="#575756"
 )
+subtitulo.pack(pady=2)
 
-tabela.heading("id", text="ID")
-tabela.heading("nome", text="Nome")
-tabela.heading("categoria", text="Categoria")
-tabela.heading("quantidade", text="Qtd")
-tabela.heading("preco", text="Preco")
 
-tabela.column("id", width=50)
-tabela.column("nome", width=160)
-tabela.column("categoria", width=140)
-tabela.column("quantidade", width=80)
-tabela.column("preco", width=80)
+def criar_label(texto):
+    label = tk.Label(
+        janela,
+        text=texto,
+        bg="#f4f6f8",
+        fg="#111827",
+        font=("Arial", 10, "bold")
+    )
+    label.pack()
 
-tabela.pack(pady=10)
 def limpar_campos():
     entrada_nome.delete(0, tk.END)
     entrada_categoria.delete(0, tk.END)
     entrada_quantidade.delete(0, tk.END)
     entrada_preco.delete(0, tk.END)
 
-
+def atualizar_tabela():
+    for item in tabela.get_children():
+        tabela.delete(item)
+    
+    #produtos = listar_produtos()
+    for produto in produtos:
+        tabela.insert("", tk.END, values=produto)
 
 def salvar():
-    nome = entrada_nome.get()
-    categoria = entrada_categoria.get()
-    quantidade = entrada_quantidade.get()
-    preco = entrada_preco.get()
+    nome = entrada_nome.get().strip()
+    categoria = entrada_categoria.get().strip()
+    quantidade = entrada_quantidade.get().strip()
+    preco = entrada_preco.get().strip()
+
     if nome == "" or categoria == "" or quantidade == "" or preco == "":
-        messagebox.showwarning("Atençao", "Preencha todos os campos")
+        messagebox.showwarning("Atenção", "Preencha todos os campos.")
         return
+
     try:
         quantidade = int(quantidade)
         preco = float(preco)
     except ValueError:
         messagebox.showwarning(
-          "Atenção",
-          "Quantidade deve ser número inteiro e preço deve usar ponto. Exemplo: 2.50"
+            "Atenção",
+            "Quantidade deve ser número inteiro e preço deve usar ponto. Exemplo: 2.50"
         )
         return
-   
-    if quantidade < 0:
-      messagebox.showwarning("Atenção", "A quantidade não pode ser negativa.")
-      return
-   
-    if preco <= 0:
-      messagebox.showwarning("Atenção", "O preço deve ser maior que zero.")
-      return
 
-    cadastrar_produto(nome, categoria, int(quantidade), float(preco))
-    messagebox.showinfo("Sucesso", "Produto cadastrar com sucesso!")
-    
-    entrada_nome.delete(0, tk.END)
-    entrada_categoria.delete(0, tk.END)
-    entrada_quantidade.delete(0, tk.END)
-    entrada_preco.delete(0, tk.END)
-    
+    if quantidade < 0:
+        messagebox.showwarning("Atenção", "A quantidade não pode ser negativa.")
+        return
+
+    if preco <= 0:
+        messagebox.showwarning("Atenção", "O preço deve ser maior que zero.")
+        return
+
+    # cadastrar_produto(nome, categoria, quantidade, preco)
+    messagebox.showinfo("Sucesso", "Produto cadastrado com sucesso!")
+    limpar_campos()
     atualizar_tabela()
 
 def excluir():
     item_selecionado = tabela.selection()
 
     if not item_selecionado:
-       messagebox.showwarning("Atenção", "Selecione um produto para excluir.")
-       return
+        messagebox.showwarning("Atenção", "Selecione um produto para excluir.")
+        return
 
     item = tabela.item(item_selecionado)
     id_produto = item["values"][0]
 
     resposta = messagebox.askyesno(
         "Confirmar exclusão",
-       "Deseja realmente excluir o produto selecionado?"
+        "Deseja realmente excluir o produto selecionado?"
     )
 
     if resposta:
-        excluir_produto(id_produto)
+        # excluir_produto(id_produto)
         atualizar_tabela()
         messagebox.showinfo("Sucesso", "Produto excluído com sucesso!")
 
 
-def atualizar_tabela():
-    for item in tabela.get_children():
-        tabela.delete(item)
-    
-    produtoLista = listar_produtos() # produtos
+criar_label("Nome do produto:")
+entrada_nome = tk.Entry(janela, width=42)
+entrada_nome.pack(pady=2)
 
-    for produto in produtoLista:
-        tabela.insert("", tk.END, values=produto)
+criar_label("Categoria:")
+entrada_categoria = tk.Entry(janela, width=42)
+entrada_categoria.pack(pady=2)
 
-atualizar_tabela()
+criar_label("Quantidade:")
+entrada_quantidade = tk.Entry(janela, width=42)
+entrada_quantidade.pack(pady=2)
 
-botao_excluir = tk.Button(
-    frame_botoes,
-    text="Excluir Produto",
-    command=excluir,
-    width=20,
-    bg="#e7490f",
-    fg="white",
-    font=("Arial", 10, "bold")
-)
-botao_excluir.grid(row=0, column=2, padx=5)
-tk.Label(janela, text="Nome do produto: ").pack()
-entrada_nome = tk.Entry(janela, width=40)
-entrada_nome.pack()
+criar_label("Preço:")
+entrada_preco = tk.Entry(janela, width=42)
+entrada_preco.pack(pady=2)
 
-tk.Label(janela, text="Categoria: ").pack()
-entrada_categoria = tk.Entry(janela, width=40)
-entrada_categoria.pack()
-
-tk.Label(janela, text="Quantidade: ").pack()
-entrada_quantidade = tk.Entry(janela, width=40)
-entrada_quantidade.pack()
-
-tk.Label(janela, text="Preco: ").pack()
-entrada_preco = tk.Entry(janela, width=40)
-entrada_preco.pack()
-
-
+frame_botoes = tk.Frame(janela, bg="#f4f6f8")
+frame_botoes.pack(pady=10)
 
 botao_salvar = tk.Button(
     frame_botoes,
@@ -152,7 +136,6 @@ botao_salvar = tk.Button(
 )
 botao_salvar.grid(row=0, column=0, padx=5)
 
-
 botao_limpar = tk.Button(
     frame_botoes,
     text="Limpar Campos",
@@ -160,4 +143,37 @@ botao_limpar = tk.Button(
     width=20
 )
 botao_limpar.grid(row=0, column=1, padx=5)
+
+botao_excluir = tk.Button(
+    frame_botoes,
+    text="Excluir Produto",
+    command=excluir,
+    width=20,
+    bg="#e7490f",
+    fg="white",
+    font=("Arial", 10, "bold")
+)
+botao_excluir.grid(row=0, column=2, padx=5)
+
+tabela = ttk.Treeview(
+    janela,
+    columns=("id", "nome", "categoria", "quantidade", "preco"),
+    show="headings"
+)
+
+tabela.heading("id", text="ID")
+tabela.heading("nome", text="Nome")
+tabela.heading("categoria", text="Categoria")
+tabela.heading("quantidade", text="Qtd")
+tabela.heading("preco", text="Preço")
+
+tabela.column("id", width=50)
+tabela.column("nome", width=180)
+tabela.column("categoria", width=160)
+tabela.column("quantidade", width=80)
+tabela.column("preco", width=100)
+
+tabela.pack(pady=10)
+
+atualizar_tabela()
 janela.mainloop()
